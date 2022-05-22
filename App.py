59bc75146c551a1c281ee5220a34019da18489d8
nevgenia0 <65017785+nevgenia0@users.noreply.github.com>
2022-05-22 12:26:37 +0300

    Add files via upload

--- App.py
+++ App.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import plotly.express as px
+from urllib.request import urlopen
+import json
 from PIL import Image
 
 # Настройка заголовка и текста
